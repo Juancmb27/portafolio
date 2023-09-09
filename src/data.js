6663297816e2a30c36596d357eda77f1ca845acb
juancmb27 <juanmontealegreb.27@gmail.com>
2023-09-09 11:02:25 -0500

    Json

---
+++ src/data.js
@@ -0,0 +1,74 @@
+export const projects =  [
+ {
+    title: "To do list",
+    subtitle: "List",
+    description:
+      "",
+    image: "./project-1.gif",
+    link: "https://to-do-list-juancmb27.vercel.app/",
+  },
+  {
+    title: "Pokedex",
+    subtitle: "Poke",
+    description:
+      "",
+    image: "./project-2.gif",
+    link: "https://b12-pokedex.netlify.app/",
+  },
+  {
+    title: "Web page",
+    subtitle: "Rafa",
+    description:
+      "",
+    image: "./project-3.gif",
+    link: "https://0227.vercel.app/",
+  },
+  {
+    title: "Criptomoneda",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "",
+  },
+  
+
+];
+
+export const skills = [
+    {
+      title: " ",
+      subtitle: "List",
+      description:
+        "",
+      image: "./project-1.gif",
+      link: "https://to-do-list-juancmb27.vercel.app/",
+    },
+    {
+      title: "Pokedex",
+      subtitle: "Poke",
+      description:
+        "",
+      image: "./project-2.gif",
+      link: "https://b12-pokedex.netlify.app/",
+    },
+    {
+      title: "Web page",
+      subtitle: "Rafa",
+      description:
+        "",
+      image: "./project-3.gif",
+      link: "https://0227.vercel.app/",
+    },
+    {
+      title: "Criptomoneda",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-4.gif",
+      link: "",
+    },
+  ];
+export const testimonials = [
+
+]
